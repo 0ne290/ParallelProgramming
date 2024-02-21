dcaed2140e8134ea8d684679c7f51384983c9c0e
Алексей <97665299+0ne290@users.noreply.github.com>
2024-02-21 23:16:21 +0800

    Update Detail.cs

--- ParallelProgramming/Detail.cs
+++ ParallelProgramming/Detail.cs
@@ -2,19 +2,44 @@ namespace ParallelProgramming;
 
 public class Detail
 {
-    public Detail(string name, int quantity, string[] machineNames, int cpuBurst)
+    public Detail(Machine[] machines, string name, int quantity, int cpuBurst)
     {
+        Machines = machines;
         Name = name;
         Quantity = quantity;
-        MachineNames = machineNames;
         CpuBurst = cpuBurst;
+        State = ProcessingStates.Queued;
     }
+
+    public async Task Process()
+    {
+        State = ProcessingStates.InProgress;
+        
+        await Task.Run(() => Machines[_machineIndex].Mill(CpuBurst, Name));
+        
+        _machineIndex++;
+        State = ProcessingStates.Queued;
+        if (_machineIndex == Machines.Length)
+        {
+            if (Quantity < 2)
+                State = ProcessingStates.Completed;
+            else
+            {
+                Quantity--;
+                _machineIndex = 0;
+            }
+        }
+    }
+
+    public Machine[] Machines { get; }
     
     public string Name { get; }
     
-    public int Quantity { get; }
-    
-    public string[] MachineNames { get; }
+    public int Quantity { get; private set; }
     
     public int CpuBurst { get; }
+
+    public ProcessingStates State { get; private set; }
+
+    private int _machineIndex;
 }
